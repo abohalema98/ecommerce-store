1912a8c10dd522588a3cb3caddbd2fee76f904bc
Mohamed Ahmed <eng.mohameda7med@gmail.com>
2021-11-16 00:16:07 +0200

    create app structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StoreHomeComponent } from './core/home/store-home/store-home.component';
+import { HeaderComponent } from './shared/header/header/header.component';
+import { FooterComponent } from './shared/footer/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StoreHomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
